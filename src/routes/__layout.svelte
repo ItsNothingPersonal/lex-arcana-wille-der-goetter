39e6e1e883e5e9820c4501a1b3b8b900e91b81c4
Sebastian Tremmel <littleperlmonk@gmail.com>
2022-01-27 01:35:55 +0100

    feat: adding tailwindcss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="bg-blue-900 shadow-lg">
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+<div class="prose lg:prose-xl prose-headings:underline">
+	<slot />
+</div>
